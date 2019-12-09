e96992fe88c7a78a93eb009205d8174e6a3589e2
HarmishaPrajapati-BTC <harmisha.prajapati@botreetechnologies.com>
2019-12-09 17:45:41 +0530

    added new images

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,15 +2,15 @@ class PostsController < ApplicationController
 
   def index
     @images = [
-      'https://cdn4.geckoandfly.com/wp-content/uploads/2014/03/motivation-motivational-quotes-poster-wallpaper5.jpg',
+      'https://quotefancy.com/media/wallpaper/3840x2160/19979-Thomas-Fuller-Quote-All-things-are-difficult-before-they-are-easy.jpg',
       'https://s-media-cache-ak0.pinimg.com/originals/1a/99/49/1a994928afa7866db6782dae8060708f.jpg',
-      'https://cdn2.geckoandfly.com/wp-content/uploads/2014/02/bill-gates-quotes3.jpg',
-      'https://cdn.geckoandfly.com/wp-content/uploads/2014/02/bill-gates-quotes4.jpg',
-      'https://cdn.geckoandfly.com/wp-content/uploads/2014/02/bill-gates-quotes7.jpg',
-      'https://cdn4.geckoandfly.com/wp-content/uploads/2016/01/jack-ma-quotes09.jpg',
-      'https://cdn3.geckoandfly.com/wp-content/uploads/2014/03/motivation-motivational-quotes-poster-wallpaper7.jpg',
-      'https://quotefancy.com/media/wallpaper/1600x900/4710-Muhammad-Ali-Quote-I-hated-every-minute-of-training-but-I-said-Don.jpg',
-      'https://cdn2.geckoandfly.com/wp-content/uploads/2014/03/leadership-management-style-skills-tips-quotes.jpg',
+      'https://quotefancy.com/media/wallpaper/3840x2160/15937-Frank-Sinatra-Quote-The-best-revenge-is-massive-success.jpg',
+      'https://quotefancy.com/media/wallpaper/3840x2160/6442-Michael-Jordan-Quote-Always-turn-a-negative-situation-into-a.jpg',
+      'https://quotefancy.com/media/wallpaper/3840x2160/19695-Milton-Berle-Quote-If-opportunity-doesn-t-knock-build-a-door.jpg',
+      'https://quotefancy.com/media/wallpaper/3840x2160/7783-Joyce-Meyer-Quote-You-cannot-have-a-positive-life-and-a-negative.jpg',
+      'https://quotefancy.com/media/wallpaper/3840x2160/14260-Jim-Rohn-Quote-Either-you-run-the-day-or-the-day-runs-you.jpg',
+      'https://quotefancy.com/media/wallpaper/3840x2160/2618-Eckhart-Tolle-Quote-The-past-has-no-power-over-the-present-moment.jpg',
+      'https://quotefancy.com/media/wallpaper/3840x2160/6361631-Charles-R-Swindoll-Quote-Life-is-10-what-happens-to-you-and-90-how.jpg',
       'https://s-media-cache-ak0.pinimg.com/originals/a4/d3/40/a4d340f895d9d32066817e81c5740ae8.jpg',
     ]
   end
