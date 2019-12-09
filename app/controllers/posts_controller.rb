392ab8b21b8c9a1c8b76eb4035de135f4567a527
HarmishaPrajapati-BTC <harmisha.prajapati@botreetechnologies.com>
2019-12-09 17:16:21 +0530

    first commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,18 @@
+class PostsController < ApplicationController
+
+  def index
+    @images = [
+      'https://cdn4.geckoandfly.com/wp-content/uploads/2014/03/motivation-motivational-quotes-poster-wallpaper5.jpg',
+      'https://s-media-cache-ak0.pinimg.com/originals/1a/99/49/1a994928afa7866db6782dae8060708f.jpg',
+      'https://cdn2.geckoandfly.com/wp-content/uploads/2014/02/bill-gates-quotes3.jpg',
+      'https://cdn.geckoandfly.com/wp-content/uploads/2014/02/bill-gates-quotes4.jpg',
+      'https://cdn.geckoandfly.com/wp-content/uploads/2014/02/bill-gates-quotes7.jpg',
+      'https://cdn4.geckoandfly.com/wp-content/uploads/2016/01/jack-ma-quotes09.jpg',
+      'https://cdn3.geckoandfly.com/wp-content/uploads/2014/03/motivation-motivational-quotes-poster-wallpaper7.jpg',
+      'https://quotefancy.com/media/wallpaper/1600x900/4710-Muhammad-Ali-Quote-I-hated-every-minute-of-training-but-I-said-Don.jpg',
+      'https://cdn2.geckoandfly.com/wp-content/uploads/2014/03/leadership-management-style-skills-tips-quotes.jpg',
+      'https://s-media-cache-ak0.pinimg.com/originals/a4/d3/40/a4d340f895d9d32066817e81c5740ae8.jpg',
+      'http://www.rd.com/wp-content/uploads/sites/2/2016/07/Motivational_quotes_olympic_coaches4.jpg'
+    ]
+  end
+end
